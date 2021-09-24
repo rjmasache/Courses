a65d8cfa3c7254903e5bd816135309eceb3f19ac
rjmasache <royerjmasache@gmail.com>
2021-09-24 00:45:03 -0500

    Comentarios y tipos de datos

---
+++ cursoJavaScript/Sintaxis/scripts.js
@@ -0,0 +1,33 @@
+// Comentario de una línea para JavaScript
+
+/**
+ * Comentario
+ * para varias líneas
+ * en JavaScript
+ */
+
+/**
+ * Tipos de datos
+ */
+
+/**
+ * number:
+ * En JavaScript todos los números pertenencen al tipo de dato number
+ */
+
+console.log(typeof 50)
+
+/**
+ * string:
+ * Cadenas de texto, se escriben entre "" o ''
+ * En JavaScrip todos los caracteres, conjunto de caracteres, espacios y vacíos pertenencen al tipo de dato string
+ */
+
+console.log(typeof 'JavaScript')
+
+/**
+ * boolean
+ * Pueden tener únicamente un valor, true or false
+ */
+
+console.log(typeof true)
