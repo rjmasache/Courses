bae9061c7fa2e240d76d31cc6b48b93021628d92
rjmasache <royerjmasache@gmail.com>
2021-09-29 17:40:36 -0500

    Operadores aritméticos, operadores de asignación

---
+++ cursoJavaScript/Operadores/scripts.js
@@ -0,0 +1,40 @@
+/**
+ * Operadores aritméticos:
+ * Permiten realizar operaciones matemáticas
+ * Suma, resta, multiplicación, división y residuo
+ */
+
+let suma = 2 + 2
+console.log(suma)
+
+let resta = 10 - 5
+console.log(resta)
+
+let multiplicacion = 5 * 5
+console.log(multiplicacion)
+
+let division = 20 / 4
+console.log(division)
+
+let residuo = 10 % 5
+console.log(residuo)
+
+/**
+ * Operadores de asignación:
+ * Permiten asignar un valor
+ */
+
+let operador = 5
+let operadorAsignacion = 10
+operador += operadorAsignacion
+console.log(operador)
+
+// Operador de concatenación
+let nombre = 'Royer'
+let apellido = 'Masache'
+let nombreCompleto = nombre + ' ' + apellido
+console.log(nombreCompleto)
+
+// Template string
+let nickname = `Mi nombre es ${nombre} y mi  apellido es ${apellido}`
+console.log(nickname)
