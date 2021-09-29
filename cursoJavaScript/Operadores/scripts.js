bdf1b27476e69a4b6b973c91c7570a2312c90677
rjmasache <royerjmasache@gmail.com>
2021-09-29 18:06:20 -0500

    Operador de asignación y Template string, Operadores de comparación

--- cursoJavaScript/Operadores/scripts.js
+++ cursoJavaScript/Operadores/scripts.js
@@ -38,3 +38,25 @@ console.log(nombreCompleto)
 // Template string
 let nickname = `Mi nombre es ${nombre} y mi  apellido es ${apellido}`
 console.log(nickname)
+
+/**
+ * Operadores de comparación:
+ * Permiten comparar datos
+ * Para datos string y numéricos: Igual y estrictamente igual, diferente y estrictamente diferente
+ */
+
+let a = 10
+let b = 20
+console.log(a === b)
+console.log(a !== b)
+
+/**
+ * Solo para datos numéricos:
+ * Mayor que, menor que,
+ * mayor o igual que, menor o igual que
+ */
+
+let c = 5
+let d = 10
+console.log(c >= d)
+console.log(c <= d)
