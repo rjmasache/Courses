8f816131e97c0cd1965af36ad250ccfd3097b37f
rjmasache <royerjmasache@gmail.com>
2021-10-01 23:10:25 -0500

    Condicional múltiple Switch y ciclo for

--- cursoJavaScript/Condicionales y ciclos/scripts.js
+++ cursoJavaScript/Condicionales y ciclos/scripts.js
@@ -1,5 +1,6 @@
 /**
- * Condicionales
+ * Condicionales:
+ * if, if else, if else if
  */
 
 /**
@@ -22,3 +23,40 @@ if (edad >= 18 && edad <= 65) {
     console.log(`${persona} no puede ir a la fiesta porque tiene ${edad} años`)
 }
 
+/**
+ * Condicionales múltiples:
+ * Switch
+ */
+
+/**
+ * ¿Qué rol en Front end eres?
+ * Si diseñas flujos de usuario eres Diseñador UI,
+ * Si llevas el diseño a código HTML y CSS eres Maquetador,
+ * Si agregas datos y lógica eres Programador Front end
+ * Si no cumples dichas características, no formas parte del Front end
+ */
+
+let rol = 'Datos y lógica'
+
+switch (rol) {
+    case 'Flujos de usuario':
+        console.log('Eres Diseñador UI')
+        break
+    case 'Diseño a código HTML y CSS':
+        console.log('Eres Maquetador')
+        break
+    case 'Datos y lógica':
+        console.log('Eres Programador Front end')
+        break
+    default:
+        console.log('No formas parte del Front end')
+}
+
+/**
+ * Ciclos:
+ * For
+ */
+
+for (let i = 0; i < 10; i++) {
+    console.log(i)
+}
