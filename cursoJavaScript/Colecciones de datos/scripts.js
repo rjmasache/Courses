4719f612a3b74b56ec0564083ef96e163cb64a5c
rjmasache <royerjmasache@gmail.com>
2021-10-05 22:59:05 -0500

    Filter, find, includes, some, every

--- cursoJavaScript/Colecciones de datos/scripts.js
+++ cursoJavaScript/Colecciones de datos/scripts.js
@@ -39,3 +39,31 @@ arreglo.forEach(usuario => console.log(`Hola ${usuario}`))
 let map = arreglo.map(usuario => `Hola ${usuario}`)
 console.log(map)
 
+/**
+ * Filter
+ * Filtra contenido de un arreglo
+ */
+
+let pares = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11]
+
+let paresFiltrados = pares.filter(numero => numero % 2 === 0)
+console.log(paresFiltrados)
+
+/**
+ * Find: Encontrar un valor
+ * Includes: Evalúa que el valor sea exactamente igual al que se busca
+ * Some: Comprueba si al menos algún elemento cumple con una condición
+ * Every: Valida si todos los elementos cumplen con una condición
+ */
+
+let find = pares.find(numero => numero % 2 !== 0)
+console.log(find)
+
+let includes = pares.includes(6)
+console.log(includes)
+
+let some = pares.some(numero => numero < 0)
+console.log(some)
+
+let every = pares.every(numero => numero === 0)
+console.log(every)
